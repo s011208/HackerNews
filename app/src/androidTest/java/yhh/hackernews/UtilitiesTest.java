50426289b959dffd8288d1c18150901315198aa4
s011208 <s011208@gmail.com>
2017-08-24 12:56:50 +0800

    remove useless comments

--- app/src/androidTest/java/yhh/hackernews/UtilitiesTest.java
+++ app/src/androidTest/java/yhh/hackernews/UtilitiesTest.java
@@ -19,11 +19,6 @@ import yhh.hackernews.utils.Utilities;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class UtilitiesTest {
     @Test
